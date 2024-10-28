af7e65778db4cbc2e0883809ae189a487176f147
nrzanita <anita.nowrouzi@gmail.com>
2024-10-28 11:56:44 +0330

    Add list

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,17 @@ function App() {
 
   return (
     <>
-        <div className="flex items-center justify-center h-screen bg-green-500 text-white">
+
+    <div className="flex items-center justify-center h-screen bg-green-500 text-white">
+
       <h1 className="text-4xl font-bold">Hello, Tailwind CSS!</h1>
       <h1 className="text-2xl">Welcome to my sersonal website</h1>
       <h1 className="text-2xl">Im anita Norouzi</h1>
+
+      <ul>
+        <li>My favorite movies</li>
+        <li>My hobbies</li>
+      </ul>
 
     </div>
     </>
