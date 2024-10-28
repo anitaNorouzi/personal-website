97016ad1ffcf90e99036ff03127a69416c526d08
Anita <anita.nowrouzi@gmail.com>
2024-10-28 15:10:47 +0330

    Delete fake lists

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,12 @@ function App() {
     <>
 
     <div className="flex items-center justify-center h-screen bg-green-500 text-white">
+      
       <h3>heloo</h3>
       <h1 className="text-4xl font-bold">Hello, Tailwind CSS!</h1>
       <h1 className="text-2xl">Welcome to my sersonal website</h1>
       <h1 className="text-2xl">Im anita Norouzi</h1>
 
-      <ul>
-        <li>My favorite movies</li>
-        <li>My hobbies</li>
-      </ul>
-
-
-      <h2>Im 23 y.o</h2>
-
-      <ul>
-
-        <li>About me</li>
-        <li>Games</li>
-        <li>Studying</li>
-
-      </ul>
     </div>
 
     </>
