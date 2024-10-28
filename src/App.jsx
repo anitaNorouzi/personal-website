625288c0db48dd8c2612434f332f72893334a300
nrzanita <anita.nowrouzi@gmail.com>
2024-10-28 11:07:55 +0330

    Add h1 tag  for my name

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
         <div className="flex items-center justify-center h-screen bg-green-500 text-white">
       <h1 className="text-4xl font-bold">Hello, Tailwind CSS!</h1>
       <h1 className="text-2xl">Welcome to my sersonal website</h1>
+      <h1 className="text-2xl">Im anita Norouzi</h1>
 
     </div>
     </>
