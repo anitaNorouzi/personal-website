aa466900655bd23e52e91779cf15e1be1296e0a1
nrzanita <anita.nowrouzi@gmail.com>
2024-10-28 12:49:42 +0330

    new fake list

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ function App() {
     <>
         <div className="flex items-center justify-center h-screen bg-green-500 text-white">
       <h1 className="text-4xl font-bold">Hello, Tailwind CSS!</h1>
+
+      <ul>
+        <li>About me</li>
+        <li>Games</li>
+        <li>Studying</li>
+      </ul>
     </div>
     </>
   )
